bdc461d6db3be2d62ff5b487f1ebf058737b4438
Harshilmehta23 <mharshil60@gmail.com>
2021-07-13 14:18:55 +0530

    initial commit

---
+++ public/js/stripe.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = Stripe(
+  'pk_test_51JCKmjSFIDKEa2Y6rVXsbPLVizxcyUMXuZidF0SSCNabCRNwtoeR43CANL0AvVhcE5lEdXz16wtbpf4isjS8RyRw00gkUm8wmx'
+);
+
+export const bookTour = async (tourId) => {
+  try {
+    // 1) Get checkout session from API
+    console.log(tourId);
+    const session = await axios(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    console.log(session);
+
+    // 2) Create checkout form + chanre credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
